50c2fa934809ec4544e22f27f5677b9e372969f2
LinuxUser68909 <anonym@anonizm.top>
2025-03-27 22:07:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
